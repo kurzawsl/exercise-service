4abd7524b44636cefb63af1708bfa6dd48ad91a0
Lukasz Kurzawski <lkurzawski@gmail.com>
2017-02-11 16:47:22 +0100

    Adding multiple output types

--- src/main/java/com/jersey/test/ActivityResource.java
+++ src/main/java/com/jersey/test/ActivityResource.java
@@ -17,7 +17,7 @@ public class ActivityResource {
 	private ActivityRepository activityRepository = new ActivityRepositoryStub();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
 	public List<Activity> getAllActivities(){
 		return activityRepository.findAllActivities();
 	}
